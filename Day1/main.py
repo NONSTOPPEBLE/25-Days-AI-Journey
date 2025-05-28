a8947419a2e1c66d5eedea2b202510c28fd6eb34
Vatsal Gavit <134800235+NONSTOPPEBLE@users.noreply.github.com>
2025-05-28 22:27:31 +0530

    Create Day1/main.py

---
+++ Day1/main.py
@@ -0,0 +1,11 @@
+print("Day 1: AI Journey Begins!")
+print("Coding on Mobile phone only")
+
+name = input("Enter your name:")
+print(f"Hello {name}! welcome to AI development!")
+
+age = int(input("your age:"))
+future_age = age + 1
+print(f"next year you'll be {future_age} and An AI expert")
+
+print("First program complete")
